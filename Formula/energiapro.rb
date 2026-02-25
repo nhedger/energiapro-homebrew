950dfa36eb603ae09c41877ff24b15abd3361c85
nhedger <649677+nhedger@users.noreply.github.com>
2026-02-25 08:13:19 +0000

    Update formula for cli-v0.1.0

--- Formula/energiapro.rb
+++ Formula/energiapro.rb
@@ -1,26 +1,26 @@
 class Energiapro < Formula
   desc "Command-line interface for the EnergiaPro API"
   homepage "https://github.com/nhedger/energiapro"
-  version "0.2.0"
+  version "0.1.0"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/nhedger/energiapro/releases/download/cli-v#{version}/energiapro-aarch64-apple-darwin"
-      sha256 "3289c8641c0238d7f841f4884e71f9f9885170f1965c34e5da4df33b1a390626"
+      sha256 "31701de012200d4e6a0e92fbc64a72ec3b27e59c29e840142d4d2dc97217d632"
     else
       url "https://github.com/nhedger/energiapro/releases/download/cli-v#{version}/energiapro-x86_64-apple-darwin"
-      sha256 "08abf0270c68998b17ff28ac764fe2b95cdf5b3d78137663280eb68543ab5403"
+      sha256 "7f2f464ca1b157f3d04387feb1eb1ff50d30b5d8af79dae10fdddacbc5abb200"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/nhedger/energiapro/releases/download/cli-v#{version}/energiapro-x86_64-unknown-linux-gnu"
-      sha256 "a0050ab1b111c1424ab32221b819f73f8c44e4bada442e5bbfc84eab5bf8fbb8"
+      sha256 "96ce3616dd392dff98e7c6ab6c780a92185fcbda16fdf83c26861e38beec8cac"
     elsif Hardware::CPU.arm?
       url "https://github.com/nhedger/energiapro/releases/download/cli-v#{version}/energiapro-aarch64-unknown-linux-gnu"
-      sha256 "8eb848fa3c16b8e325ac7b15fcb6cbb71b364042269f2181d1b0aed0ab0cf8a2"
+      sha256 "5ce54899daffcfa055c01122676db1643a48331a17bad5fb347f48c2af3b119d"
     else
       odie "Unsupported Linux architecture: #{Hardware::CPU.arch}"
     end
